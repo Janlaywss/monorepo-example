14e9158492a6ef8f4c6783d416eede126baab841
janlay <janlaywss@gmail.com>
2022-05-05 15:44:30 +0800

    chore: repo依赖统一

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // 单行最大字数
+  printWidth: 120,
+  //句末加分号
+  semi: true,
+  // 统一使用单引号，
+  // jsx内属性用双引号，后面有配置
+  singleQuote: true,
+  // 在换了行的数组,对象项末尾加 ,
+  trailingComma: 'all',
+  // 在{} 前后都加空格， 例如{ a: 1, b: 2 }
+  bracketSpacing: true,
+  // 多行jsx 以 >为最后一行
+  bracketSameLine: false,
+  // 为单行箭头函数的单个参数，不需要加括号
+  arrowParens: 'avoid',
+  // 缩进 2个空格
+  tabWidth: 2,
+  // 不使用Tab缩进，而使用空格
+  useTabs: false,
+  // 文件结尾要换行
+  endOfLine: 'auto',
+};
--- packages/repo-app/next.config.js
+++ packages/repo-app/next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
+const hasha = require('hasha');
+const path = require('path');
+
 const nextConfig = {
-  reactStrictMode: true
+  cssModules: {
+    getLocalIdent: ({ resourcePath }, localIdentName, localName) => {
+      if (/\.global\.(css|less)$/.test(resourcePath) || /node_modules/.test(resourcePath)) {
+        return localName;
+      }
+      return `${localName}__${hasha(resourcePath + localName, { algorithm: 'md5' }).slice(0, 8)}`;
+    }
+  },
+  webpack(config) {
+    config.resolve.alias.react = path.resolve(__dirname, './node_modules/react');
+    config.resolve.alias['react-dom'] = path.resolve(__dirname, './node_modules/react-dom');
+    config.module.rules.push(
+        {
+          test: /\.svg$/,
+          issuer: /\.(js|ts)x|less|css?$/,
+          use: ['@svgr/webpack']
+        },
+        {
+          test: /\.(eot|woff|woff2|ttf|png|jpg|gif)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              name: '[name].[ext]'
+            }
+          }
+        },
+    );
+    return config;
+  }
 }
 
 module.exports = nextConfig
